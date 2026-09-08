af4b03b49628c9338074a828f5861004205c4a59
silentk1811 <kalyanip18115@gmail.com>
2026-09-08 07:55:02 +0530

    oops

---
+++ Bankaccount.py
@@ -0,0 +1,10 @@
+class BankAccount:
+    def deposit(self):
+        print("Amount deposited")
+
+    def withdraw(self):
+        print("Amount withdrawn")
+
+bank = BankAccount()
+bank.deposit()
+bank.withdraw()
---
+++ Employee.py
@@ -0,0 +1,8 @@
+class Employee:
+    def display(self):
+        print("Name: Kalyani")
+        print("Salary: 30000")
+        print("Department: IT")
+
+emp = Employee()
+emp.display()
